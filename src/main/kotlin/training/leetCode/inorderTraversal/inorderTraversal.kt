7c3f87e5d1ad456a7059a33917fbdcecf1328434
Daniel Amorim <daniel.amorim@farfetch.com>
2025-06-27 22:08:29 +0100

    Leetcode: Binary tree inorder traversal

---
+++ src/main/kotlin/training/leetCode/inorderTraversal/inorderTraversal.kt
@@ -0,0 +1,33 @@
+package training.leetCode.inorderTraversal
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        val result = mutableListOf<Int>()
+        inorder(root, result)
+        return result
+    }
+
+    private fun inorder(node: TreeNode?, list: MutableList<Int>) {
+        if(node == null) return
+        inorder(node.left, list)
+        list.add(node.`val`)
+        inorder(node.right, list)
+    }
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+
+    companion object
+}
